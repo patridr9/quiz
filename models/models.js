4cc9052ffc6c885f8f922ee7d099d44547bd764c
Patricia Dopico <aninonino@gmail.com>
2015-07-08 13:57:09 +0200

    Adaptación para Multiples preguntas

--- models/models.js
+++ models/models.js
@@ -3,42 +3,46 @@ var path = require('path');
 //postgres DATABASE_URL = postgres://user:password@host:port/database
 //SQLite DATABASE_URL = sqlite://:@:/
 var url = process.env.DATABASE_URL.match(/(.*)\:\/\/(.*?)\:(.*)@(.*)\:(.*)\/(.*)/);
-var DB_name 	= (url[6]|| null);
-var user 		=	(url[2]|| null);
-var pwd			= (url[3]|| null);
-var protocol 	= (url[1]|| null);
-var dialect		= (url[1]|| null);
-var port 		= (url[5]|| null);
-var host 		= (url[4]|| null);
-var storage 	= process.env.DATABASE_STORAGE;
+var DB_name  = (url[6]||null);
+var user     = (url[2]||null);
+var pwd      = (url[3]||null);
+var protocol = (url[1]||null);
+var dialect  = (url[1]||null);
+var port     = (url[5]||null);
+var host     = (url[4]||null);
+var storage  = process.env.DATABASE_STORAGE;
 
 //Cargar modelo ORM
 var Sequelize = require('sequelize');
 
 //usar BBDD SQLite
-var sequelize = new Sequelize(DB_name, user, pwd,
-					{ dialect: protocol, 
-						protocol: protocol,
-						port : port,
-						host : host,
-						storage: storage,
-						omitNull: true
-					}
-				);
+var sequelize = new Sequelize(DB_name, user, pwd, 
+  { dialect:  protocol,
+    protocol: protocol,
+    port:     port,
+    host:     host,
+    storage:  storage,  // solo SQLite (.env)
+    omitNull: true      // solo Postgres
+  }      
+);
 
 //Importar la defincion de la tabla Quiz en quiz.js
 var Quiz = sequelize.import(path.join(__dirname, 'quiz'));
 exports.Quiz = Quiz; //Exportar defincion tabla Quiz
 
 //sequelize.sync() crea e inicializa tabla de preguntas en BD
-sequelize.sync().success(function(){
-	Quiz.count().success(function (count){
+sequelize.sync().then(function(){
+	Quiz.count().then(function (count){
 		if (count === 0) {
 			Quiz.create({ 
 						pregunta: 'Capital de Italia',
 						respuesta: 'Roma'
+			});
+			Quiz.create({ 
+						pregunta: 'Capital de Portugal',
+						respuesta: 'Lisboa'
 			})
-			.success(function(){
+			.then(function(){
 				console.log('Base de datos inicializada');
 			});
 		}
